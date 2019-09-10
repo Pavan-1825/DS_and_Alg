ff7e4032b5332518e39d741d2909f14d2be1cfb2
Pavankumar Kaareddy - Vendor <Pavankumar.Kaareddy@walmartlabs.com>
2019-09-10 02:29:13 -0700

    GameEntry Exercise 1.2

--- src/Test/chapter_1_Test/SumOfSquareRootNumTest.java
+++ src/Test/chapter_1_Test/SumOfSquareRootNumTest.java
@@ -2,13 +2,28 @@ package Test.chapter_1_Test;
 
 import Assignments.chapter_1.SumOfSquareRootNum;
 
+import java.util.Scanner;
+
 public class SumOfSquareRootNumTest {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int x = 11;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number :");
+        int num = input.nextInt();
 
-        System.out.println("Answer :" + SumOfSquareRootNum.sumOfSquares1(4));
-        System.out.println("Answer :" + SumOfSquareRootNum.sumOfSquares2(4));
+        System.out.println("Method_1 Answer :" + SumOfSquareRootNum.sumOfSquares1(num));
+        System.out.println("Method_2 Answer :" + SumOfSquareRootNum.sumOfSquares2(num));
+
+        // Logic to find the number's that are square root of itself for integer n, eg: 1, 4, 9 are Square root of itself for given number 11
+        /*for (int x = 1; x <= i; x++) {
+            for (int y = 1; y <= i; y++) {
+                if (y * y == x) {
+                    System.out.print(x+"\n");
+                    sum += x;
+                }
+            }
+        }
+        System.out.println("\n sum of all: "+sum);*/
     }
 }
